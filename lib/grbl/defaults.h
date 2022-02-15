c55d3839c5041cffc3e247898f5f832f3f08abbd
HotWire CNC <hotwire.cnc@gmail.com>
2022-02-15 19:40:18 +0000

    DEFAULT_HOMING_PULLOFF 2 mm

--- lib/grbl/defaults.h
+++ lib/grbl/defaults.h
@@ -509,7 +509,7 @@
   #define DEFAULT_HOMING_FEED_RATE 200.0 // mm/min
   #define DEFAULT_HOMING_SEEK_RATE 100.0 // mm/min
   #define DEFAULT_HOMING_DEBOUNCE_DELAY 250 // msec (0-65k)
-  #define DEFAULT_HOMING_PULLOFF 5.0 // mm
+  #define DEFAULT_HOMING_PULLOFF 2.0 // mm
 #endif
 
 #endif
